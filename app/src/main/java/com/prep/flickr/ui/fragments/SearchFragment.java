f77e8dec41d8fe9b61606d63820b66cb26a66872
Nyjal Augustine <nyjal@box.com>
2019-08-24 02:16:24 -0700

    Prepare layout for Search Fragment.

--- app/src/main/java/com/prep/flickr/ui/fragments/SearchFragment.java
+++ app/src/main/java/com/prep/flickr/ui/fragments/SearchFragment.java
@@ -4,9 +4,13 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ProgressBar;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SearchView;
+import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
@@ -18,31 +22,64 @@ import com.prep.flickr.ui.viewmodels.SearchViewModel;
 public class SearchFragment extends Fragment {
 
     private SearchViewModel mViewModel;
+    private Toolbar mToolbar;
+    private SearchView mSearchView;
+    private ProgressBar mProgressBar;
 
     public static SearchFragment newInstance() {
         return new SearchFragment();
     }
 
     @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_search, container, false);
+        View view = inflater.inflate(R.layout.fragment_search, container, false);
+        mSearchView = view.findViewById(R.id.search_view);
+        mToolbar = view.findViewById(R.id.toolbar);
+        mProgressBar = view.findViewById(R.id.progress_loading);
+        ((AppCompatActivity)getActivity()).setSupportActionBar(mToolbar);
+        initSearchView();
+        return view;
+    }
+
+    private void initSearchView() {
+        mSearchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                // Clear focus as soon as query is submitted otherwise the focus stays on
+                // and if we click back the click will be used to remove focus and not actually
+                // perform back navigation
+                mSearchView.clearFocus();
+
+                mViewModel.searchPhotos(query, 1);
+                setProgressBarVisibility(true);
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return false;
+            }
+        });
     }
 
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
         mViewModel = ViewModelProviders.of(this).get(SearchViewModel.class);
-        initObservers();
-
-        mViewModel.searchPhotos("mole rat", 15);
+        subscribeObservers();
     }
 
-    private void initObservers() {
+    private void subscribeObservers() {
         mViewModel.getPhotos().observe(getViewLifecycleOwner(), new Observer<FlickrPhotosResponse>() {
             @Override
             public void onChanged(FlickrPhotosResponse flickrPhotosResponse) {
-
+                setProgressBarVisibility(false);
             }
         });
 
@@ -52,5 +89,10 @@ public class SearchFragment extends Fragment {
 
             }
         });
+    }
+
+    private void setProgressBarVisibility(boolean show) {
+        int visibility = (show ? View.VISIBLE : View.GONE);
+        mProgressBar.setVisibility(visibility);
     }
 }
